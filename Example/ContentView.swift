1ffaa066eb62d58108bbae56c1c1086a30e17c45
victormwenda <kiambi15@gmail.com>
2020-06-23 11:38:25 +0300

    Initial Commit

---
+++ Example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Example
+//
+//  Created by Victor Kiambi on 6/23/20.
+//  Copyright © 2020 Victor Kiambi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
